411c95c5cd727232eeca05951e4ca2ec703b6c0e
Prof-Ursi <8743@holbertonstudents.com>
2024-09-26 10:57:53 +0200

    Updating main.ts in task_1/ for task 4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,4 +1,4 @@
-// Create interface named Teacher
+// task 1: Create interface named Teacher
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
@@ -8,7 +8,49 @@ interface Teacher {
     [attributeName: string]: any;
 }
 
-// Create interface named Directors extending Teacher
+// task 2: Create interface named Directors extending Teacher
 interface Directors extends Teacher {
     numberOfReports: number;
+}
+
+// task 3: Creat interface named printTeacherFunction for a function taking firstName and lastName as arguments and returning a string
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+
+// task 4: Create an interface for the class StudentClass
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+//task 4: Create an interface for the class StudentClass constructor
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+
+// task 3: Create a function named printTeacher using the printTeacherFunction interface
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+// task 4: Create a class named StudentClass
+class StudentClass implements StudentClassInterface {
+    firstName;
+    lastName;
+    constructor({firstName, lastName}: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework() {
+        return 'Currently working';
+    }
+    displayName() {
+        return this.firstName;
+    }
 }
