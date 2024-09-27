be12af412072012bb46aaa64bd1c61194e3e8393
Prof-Ursi <8743@holbertonstudents.com>
2024-09-27 11:38:08 +0200

    Adding task_5/ directory with everything inside for task 10

---
+++ task_5/js/main.ts
@@ -0,0 +1,27 @@
+// Create MajorCredits interface
+interface MajorCredits {
+    credits: number;
+    brand: 'MajorCredits';
+}
+
+// Create MinorCredits interface
+interface MinorCredits {
+    credits: number;
+    brand: 'MinorCredits';
+}
+  
+// Create function to sum major credits
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        brand: 'MajorCredits',
+    };
+}
+
+//Create function to sum minor credits
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        brand: 'MinorCredits',
+    };
+}
